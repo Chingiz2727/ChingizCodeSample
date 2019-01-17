b2941123fe38573fe2d9b2dcba6d954d4f2b12c7
Chingiz2727 <k.chyngiz@gmail.com>
2019-01-17 10:24:53 +0600

    Initial Commit

---
+++ ChyngizCodeSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChyngizCodeSample
+//
+//  Created by Чингиз on 1/17/19.
+//  Copyright © 2019 Чингиз. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
